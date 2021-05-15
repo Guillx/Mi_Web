bf096f37273a4aa0066f4b0dac0c9eeec93774e7
Guillx <guillx.gv@gmail.com>
2021-05-15 10:15:59 +0200

    portfolio component completo + responsive

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,90 +1,285 @@
 import "../assets/css/Portfolio.css";
-import nopicimg from "../assets/images/nopic.jpg";
-// import netflix from "../assets/images/netflix.jpg";
+
+import netflix from "../assets/images/pixabay.jpeg";
 import picmovies from "../assets/images/pic-movies.png";
 import gifs from "../assets/images/gifs.png";
+import nopicimg from "../assets/images/nopic.jpg";
 
+// FONT AWESOME
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
 
+// POPUPBOX
+import { PopupboxManager, PopupboxContainer } from "react-popupbox";
+import "react-popupbox/dist/react-popupbox.css";
+
 const Portfolio = () => {
+  // proyecto 1
+  const openPopupboxProyecto1 = () => {
+    const content = (
+      <>
+        <h2>Proyecto 1</h2>
+        <img className="portfolio-image-popupbox" src={netflix} alt=""></img>
+        <p className="portfolio-text-popupbox">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dui
+          eros, faucibus ac enim ac, venenatis facilisis turpis. Aliquam erat
+          volutpat. Aliquam varius venenatis dolor. Duis sed cursus nibh.
+          Maecenas placerat laoreet tellus, sit amet rutrum neque fringilla
+          quis. Aliquam euismod elit quis dignissim volutpat. Sed et iaculis
+          odio, id.{" "}
+        </p>
+        <b>GitHub: </b>
+        <a
+          href="https://github.com/Guillx"
+          className="hyper-link-popupbox"
+          onClick={() => window.open("https://github.com/Guillx")}
+        >
+          https://github.com/Guillx
+        </a>
+      </>
+    );
+    PopupboxManager.open({ content });
+  };
+  // const popupboxConfigProyecto1 = {
+  //   titleBar: {
+  //     enable: true,
+  //     text: "Título del proyecto 1",
+  //   },
+  //   fade: true,
+  //   fadeInSpeed: 500,
+  // };
+
+  // proyecto 2
+  const openPopupboxProyecto2 = () => {
+    const content = (
+      <>
+        <h2>Buscador de películas</h2>
+        <img className="portfolio-image-popupbox" src={picmovies} alt=""></img>
+        <p className="portfolio-text-popupbox">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dui
+          eros, faucibus ac enim ac, venenatis facilisis turpis. Aliquam erat
+          volutpat. Aliquam varius venenatis dolor. Duis sed cursus nibh.
+          Maecenas placerat laoreet tellus, sit amet rutrum neque fringilla
+          quis. Aliquam euismod elit quis dignissim volutpat. Sed et iaculis
+          odio, id.{" "}
+        </p>
+        <b>GitHub: </b>
+        <a
+          href="https://github.com/Guillx/buscador_peliculas"
+          className="hyper-link-popupbox"
+          onClick={() =>
+            window.open("https://github.com/Guillx/buscador_peliculas")
+          }
+        >
+          https://github.com/Guillx/buscador_peliculas
+        </a>
+      </>
+    );
+    PopupboxManager.open({ content });
+  };
+
+  // proyecto 3
+  const openPopupboxProyecto3 = () => {
+    const content = (
+      <>
+        <h2>Buscador de giffs</h2>
+        <img className="portfolio-image-popupbox" src={gifs} alt=""></img>
+        <p className="portfolio-text-popupbox">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dui
+          eros, faucibus ac enim ac, venenatis facilisis turpis. Aliquam erat
+          volutpat. Aliquam varius venenatis dolor. Duis sed cursus nibh.
+          Maecenas placerat laoreet tellus, sit amet rutrum neque fringilla
+          quis. Aliquam euismod elit quis dignissim volutpat. Sed et iaculis
+          odio, id.{" "}
+        </p>
+        <b>GitHub: </b>
+        <a
+          href="https://github.com/Guillx/giffy"
+          className="hyper-link-popupbox"
+          onClick={() => window.open("https://github.com/Guillx/giffy")}
+        >
+          https://github.com/Guillx/giffy
+        </a>
+      </>
+    );
+    PopupboxManager.open({ content });
+  };
+
+  // proyecto 4
+  const openPopupboxProyecto4 = () => {
+    const content = (
+      <>
+        <h2>Otro proyecto</h2>
+        <img className="portfolio-image-popupbox" src={nopicimg} alt=""></img>
+        <p className="portfolio-text-popupbox">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dui
+          eros, faucibus ac enim ac, venenatis facilisis turpis. Aliquam erat
+          volutpat. Aliquam varius venenatis dolor. Duis sed cursus nibh.
+          Maecenas placerat laoreet tellus, sit amet rutrum neque fringilla
+          quis. Aliquam euismod elit quis dignissim volutpat. Sed et iaculis
+          odio, id.{" "}
+        </p>
+        <b>GitHub: </b>
+        <a
+          href="https://github.com/Guillx/giffy"
+          className="hyper-link-popupbox"
+          onClick={() => window.open("https://github.com/Guillx/giffy")}
+        >
+          https://github.com/Guillx/giffy
+        </a>
+      </>
+    );
+    PopupboxManager.open({ content });
+  };
+
   return (
-    <div id="portfolio" className="portfolio-container">
+    <div id="portfolio" className="portfolio-wrapper">
       <div className="portfolio-texto-container">
         <h1 className="portfolio-titulo">Portfolio</h1>
         <p className="portfolio-texto">
-          En esta sección te presento los trabajos que he realizado para
-          aprender y practicar programación web.
-          <br></br>Al hacer clic en cada uno de ellos, podrás ver un vídeo, el
-          link a GitHub y una descripción más detallada del proyecto.
+          En esta sección te presento alguno de los trabajos que he realizado
+          para aprender y practicar programación y diseño web. Al hacer clic en
+          cada uno de ellos, podrás ver un vídeo, el link a GitHub y una
+          descripción más detallada del proyecto.<br></br>
+          <br></br> Como verás, la mayoría de las prácticas que he realizado son
+          de frontend. Creo que a corto-medio plazo, dónde más puedo aportar y
+          dónde más cómodo me sentiré es en frontend y diseño, pero también me
+          me encantaría aprender backend.
         </p>
       </div>
-
       <div className="portfolio-cards-container">
-        <div className="card-container">
-          <a href="/">
-            <img className="img-card" src={picmovies} alt="img-card"></img>
-            <h3 className="portfolio-card-title">Buscador de películas</h3>
-            <p className="portfolio-card-description">
-              Web realizada con la API de imDb
-            </p>
-
+        <div className="image-box-wrapper row justify-content-center">
+          <div className="portfolio-image-box" onClick={openPopupboxProyecto1}>
+            <img className="portfolio-image" src={netflix} alt=""></img>
+            <div className="overflow"></div>
             <FontAwesomeIcon
               className="portfolio-icon"
               icon={faSearchPlus}
             ></FontAwesomeIcon>
-          </a>
-        </div>
+          </div>
 
-        <div className="card-container">
-          <a href="/">
-            <img className="img-card" src={gifs} alt="img-card"></img>
-            <h3 className="portfolio-card-title">Giffy Clone</h3>
-            <p className="portfolio-card-description">
-              Web de Gifs con la API de Ghiffy.
-            </p>
-
+          <div className="portfolio-image-box" onClick={openPopupboxProyecto2}>
+            <img className="portfolio-image" src={picmovies} alt=""></img>
+            <div className="overflow"></div>
             <FontAwesomeIcon
               className="portfolio-icon"
               icon={faSearchPlus}
             ></FontAwesomeIcon>
-          </a>
-        </div>
+          </div>
 
-        <div className="card-container">
-          <a href="/">
-            <img className="img-card" src={nopicimg} alt="img-card"></img>
-            <h3 className="portfolio-card-title">Lorem Ipsum 4</h3>
-            <p className="portfolio-card-description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a
-              fermentum enim.
-            </p>
-
+          <div className="portfolio-image-box" onClick={openPopupboxProyecto3}>
+            <img className="portfolio-image" src={gifs} alt=""></img>
+            <div className="overflow"></div>
             <FontAwesomeIcon
               className="portfolio-icon"
               icon={faSearchPlus}
             ></FontAwesomeIcon>
-          </a>
-        </div>
+          </div>
 
-        <div className="card-container">
-          <a href="/">
-            <img className="img-card" src={nopicimg} alt="img-card"></img>
-            <h3 className="portfolio-card-title">Lorem Ipsum 5</h3>
-            <p className="portfolio-card-description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a
-              fermentum enim.
-            </p>
-
+          <div className="portfolio-image-box" onClick={openPopupboxProyecto4}>
+            <img className="portfolio-image" src={nopicimg} alt=""></img>
+            <div className="overflow"></div>
             <FontAwesomeIcon
               className="portfolio-icon"
               icon={faSearchPlus}
             ></FontAwesomeIcon>
-          </a>
+          </div>
         </div>
       </div>
+      {/* <PopupboxContainer {...popupboxConfigProyecto1}></PopupboxContainer>
+      <PopupboxContainer {...popupboxConfigProyecto2}></PopupboxContainer>
+      <PopupboxContainer {...popupboxConfigProyecto3}></PopupboxContainer>
+      <PopupboxContainer {...popupboxConfigGiffy}></PopupboxContainer> */}
     </div>
   );
 };
+// import "../assets/css/Portfolio.css";
+// import nopicimg from "../assets/images/nopic.jpg";
+// // import netflix from "../assets/images/netflix.jpg";
+// import picmovies from "../assets/images/pic-movies.png";
+// import gifs from "../assets/images/gifs.png";
+
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
+
+// const Portfolio = () => {
+//   return (
+//     <div id="portfolio" className="portfolio-container">
+//       <div className="portfolio-texto-container">
+//         <h1 className="portfolio-titulo">Portfolio</h1>
+//         <p className="portfolio-texto">
+//           En esta sección te presento los trabajos que he realizado para
+//           aprender y practicar programación web.
+//           <br></br>Al hacer clic en cada uno de ellos, podrás ver un vídeo, el
+//           link a GitHub y una descripción más detallada del proyecto.
+//         </p>
+//       </div>
+
+//       <div className="portfolio-cards-container">
+//         <div className="card-container">
+//           <a href="/">
+//             <img className="img-card" src={picmovies} alt="img-card"></img>
+//             <h3 className="portfolio-card-title">Buscador de películas</h3>
+//             <p className="portfolio-card-description">
+//               Web realizada con la API de imDb
+//             </p>
+
+//             <FontAwesomeIcon
+//               className="portfolio-icon"
+//               icon={faSearchPlus}
+//             ></FontAwesomeIcon>
+//           </a>
+//         </div>
+
+//         <div className="card-container">
+//           <a href="/">
+//             <img className="img-card" src={gifs} alt="img-card"></img>
+//             <h3 className="portfolio-card-title">Giffy Clone</h3>
+//             <p className="portfolio-card-description">
+//               Web de Gifs con la API de Ghiffy.
+//             </p>
+
+//             <FontAwesomeIcon
+//               className="portfolio-icon"
+//               icon={faSearchPlus}
+//             ></FontAwesomeIcon>
+//           </a>
+//         </div>
+
+//         <div className="card-container">
+//           <a href="/">
+//             <img className="img-card" src={nopicimg} alt="img-card"></img>
+//             <h3 className="portfolio-card-title">Lorem Ipsum 4</h3>
+//             <p className="portfolio-card-description">
+//               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a
+//               fermentum enim.
+//             </p>
+
+//             <FontAwesomeIcon
+//               className="portfolio-icon"
+//               icon={faSearchPlus}
+//             ></FontAwesomeIcon>
+//           </a>
+//         </div>
+
+//         <div className="card-container">
+//           <a href="/">
+//             <img className="img-card" src={nopicimg} alt="img-card"></img>
+//             <h3 className="portfolio-card-title">Lorem Ipsum 5</h3>
+//             <p className="portfolio-card-description">
+//               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a
+//               fermentum enim.
+//             </p>
+
+//             <FontAwesomeIcon
+//               className="portfolio-icon"
+//               icon={faSearchPlus}
+//             ></FontAwesomeIcon>
+//           </a>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default Portfolio;
--- src/components/Presentacion.js
+++ src/components/Presentacion.js
@@ -1,21 +1,62 @@
 import "../assets/css/Presentacion.css";
 
+// POPUPBOX
+import { PopupboxManager, PopupboxContainer } from "react-popupbox";
+import "react-popupbox/dist/react-popupbox.css";
+
 const Presentacion = () => {
+  //
+  const openPopupboxMasInfo = () => {
+    const content = (
+      <>
+        <h2>Más sobre mí</h2>
+        <p className="portfolio-text-popupbox">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dui
+          eros, faucibus ac enim ac, venenatis facilisis turpis. Aliquam erat
+          volutpat. Aliquam varius venenatis dolor. Duis sed cursus nibh.
+          Maecenas placerat laoreet tellus, sit amet rutrum neque fringilla
+          quis. Aliquam euismod elit quis dignissim volutpat. Sed et iaculis
+          odio, id.{" "}
+        </p>
+        <b>GitHub: </b>
+        <a
+          href="https://github.com/Guillx"
+          className="hyper-link-popupbox"
+          onClick={() => window.open("https://github.com/Guillx")}
+        >
+          https://github.com/Guillx
+        </a>
+      </>
+    );
+    PopupboxManager.open({ content });
+  };
+  const popupboxConfigMasInfo = {
+    fade: true,
+    fadeInSpeed: 500,
+  };
+
   return (
     <div id="presentacion" className="presentacion-container">
       <h1 className="presentacion-titulo">Presentación</h1>
       <p className="presentacion-texto">
         ¡Hola, me llamo Guille! <br></br>
-        Hasta que llegó el Covid me dedicaba a la hostelería, trabajando
-        mayormente de cocinero. Al ver que la "nueva normalidad" iba para largo,
-        en septiembre de 2020 me puse a estudiar un Bootcamp de programación
-        web, sin saber muy bien dónde me metía, ya que no tenía ni idea de como
-        se hacía una página web. Pero aquí estoy, presentándote la mía propia.{" "}
+        Hasta que llegó el Covid trabajaba de cocinero, pero al ver que la
+        "nueva normalidad" iba para largo, en septiembre de 2020 me puse a
+        estudiar un Bootcamp de programación web, sin saber muy bien dónde me
+        metía, ya que no tenía ni idea de como se hacía una página web. Tras
+        unos meses de aprendizaje, he descubierto un mundo nuevo, que me gusta y
+        en el que quiero quedarme. <br></br>
+        <br></br> De momento aún estoy muy verde, pero con tan solo 4 meses he
+        conseguido sacar adelante un proyecto que considero bastante grande e
+        importante para ser el primero.
         <br></br>
-        <br></br> De momento aún estoy muy verde, pero con tan solo 4 meses de
-        aprendizaje he conseguido sacar adelante un proyecto que considero
-        bastante grande e importante para ser el primero y es lo que te voy a
-        enseñar en el vídeo que aparece a continuación.
+        Te presento mi proyecto de fin de Bootcamp: una aplicación web de compra
+        y venta de tecnología retro.
+        <br></br>
+        Este proyecto fue realizado en poco más de 2 meses y partiendo de cero.
+        Entre yo y un compañero de clase hemos creado la base de datos, el
+        backend y el frontend. En el vídeo que aparece a continuación podrás ver
+        "RetroTech" en funcionamiento y, además, podrás conocerme un poco mejor.
         <br></br>
         <br></br>No me enrollo más. ¡Dale al PLAY!
       </p>
@@ -31,14 +72,19 @@ const Presentacion = () => {
           allowfullscreen
         ></iframe>
       </div>
-      {/* 
+
       <p className="presentacion-texto">
         Si quieres saber más cosas sobre mi, haz clic{" "}
-        <a className="link-presentacion-texto" href="/">
+        <span
+          className="link-presentacion-texto"
+          href="/"
+          onClick={openPopupboxMasInfo}
+        >
           AQUÍ
-        </a>
+        </span>
         .
-      </p> */}
+      </p>
+      <PopupboxContainer {...popupboxConfigMasInfo}></PopupboxContainer>
     </div>
   );
 };
