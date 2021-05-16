1d42fb4d77388e2028bda2c3460a1f1cf3ef19f7
Guillx <guillx.gv@gmail.com>
2021-05-16 05:26:45 +0200

    footer falta responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,75 @@
 import "../assets/css/Footer.css";
+import githubicon from "../assets/images/github.svg";
+import linkedinicon from "../assets/images/linkedin.svg";
 
 const Footer = () => {
-  return <div className="footer-container"></div>;
+  return (
+    <div className="footer-container">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 col-md-6 col-sm-6">
+            <div className="d-flex">
+              <p>15117, Laxe, A Coruña</p>
+            </div>
+            <div className="d-flex">
+              <a href="tel:625440474">+34 625 440 474</a>
+            </div>
+            <div className="d-flex">
+              <p>guillx.gv@gmail.com</p>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-2 col-sm-6">
+            <div className="row">
+              <div className="col">
+                <a href="/" className="footer-nav">
+                  Inicio
+                </a>
+                <br></br>
+                <a href="/" className="footer-nav">
+                  Presentación
+                </a>
+                <br></br>
+                <a href="/" className="footer-nav">
+                  Habilidades
+                </a>
+              </div>
+
+              <div className="col">
+                <a href="/" className="footer-nav">
+                  Experiencia
+                </a>
+                <br></br>
+                <a href="/" className="footer-nav">
+                  Portfolio
+                </a>
+                <br></br>
+                <a href="/" className="footer-nav">
+                  Contacto
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="links-social-footer">
+            <div className="d-flex">
+              <a href="https://github.com/Guillx">
+                <img className="icon-footer" src={githubicon} alt=""></img>
+                <p>GitHub</p>
+              </a>
+              <a href="https://www.linkedin.com/in/guillermo-gil-vazquez-link/">
+                <img className="icon-footer" src={linkedinicon} alt=""></img>
+                <p>Linkedin</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <p className="copyright-text-footer">
+          Copyright &copy; {new Date().getFullYear()}&nbsp;Guillx | All Rights
+          Reserved
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
